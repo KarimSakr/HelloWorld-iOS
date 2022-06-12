74396386863f192610a5cb648dae10f26b63126e
Karim Sakr <89774076+KarimSakr@users.noreply.github.com>
2022-06-12 19:54:53 +0300

    Update ContentView.swift

--- SwiftUI./ContentView.swift
+++ SwiftUI./ContentView.swift
@@ -8,11 +8,15 @@ struct ContentView: View {
  var body: some View {
    //Text() lets you write text
    //.padding() and .backgrounf are modifiers, they modify the elements 
-   Text("Hello, World!").padding().background(Color.blue.blur(radius: 3.0))
+   Text("Hello, World!")
+  .padding()
+  .background(Color.blue.blur(radius: 3.0))
    
    //Image() lets you add images
    //logo is the name of the image
-   Image("logo").resizable().aspectRatio(contentMode: .fit)
+   Image("logo")
+  .resizable()
+  .aspectRatio(contentMode: .fit)
  }
 }
 
